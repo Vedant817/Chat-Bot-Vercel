6705b61d689d0b0174b743ecd8ea8f6b89ea6ebb
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-06-15 22:43:51 +0530

    Changing the Font

--- src/compoments/NavBar.tsx
+++ src/compoments/NavBar.tsx
@@ -7,11 +7,11 @@ const NavBar = () => {
     return (
         <nav className='p-4 md:p-6 shadow-md bg-zinc-800'>
             <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
-            <a className='text-xl font-bold mb-4 md:mb-0 text-white' href='/'>
+            <a className='text-2xl font-bold mb-4 md:mb-0 text-white font-mono' href='/'>
                 <span>Chat Bot: {' '}</span>
                 <span className='font-normal'>Your daily Assistant</span>
             </a>
-            <Link href='/sign-in'><Button className='w-full md:w-auto text-white bg-green-600 hover:bg-green-800'>Login</Button></Link>
+            <Link href='/sign-in'><Button className='w-full md:w-auto text-white bg-green-600 hover:bg-green-800 font-mono'>Login</Button></Link>
             </div>
         </nav>
     )
