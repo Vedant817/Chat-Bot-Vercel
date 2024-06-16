07fc7ea6b8ba8f7744030473e0d7873e4ebdbe29
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-06-17 01:04:36 +0530

    Bug Fixing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ import { account } from '@/lib/appwrite'
 
 const NavBar = () => {
     const [session, setSession] = useState(null)
-    useEffect(()=>{
+    useEffect(() => {
         const checkLogin = async () => {
             const Session = await account.get();
             setSession(Session)
@@ -16,17 +16,17 @@ const NavBar = () => {
     return (
         <nav className='p-4 md:p-6 shadow-md bg-black'>
             <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
-            <a className='text-2xl font-bold mb-4 md:mb-0 text-white font-mono' href='/'>
-                <span>Chat Bot: {' '}</span>
-                <span className='font-normal'>Your daily Assistant</span>
-            </a>
-            <Link href='/sign-in'>{
-                session ? (
-                    <Button className='w-full md:w-auto text-white bg-green-600 hover:bg-green-800 font-mono'>Save</Button>
-                ) : (
-                    <Button className='w-full md:w-auto text-white bg-green-600 hover:bg-green-800 font-mono'>Login</Button>
-                )
-            }</Link>
+                <a className='text-2xl font-bold mb-4 md:mb-0 text-white font-mono' href='/'>
+                    <span>Chat Bot: {' '}</span>
+                    <span className='font-normal'>Your daily Assistant</span>
+                </a>
+                {
+                    session ? (
+                        <Link href='/savedChats'><Button className='w-full md:w-auto text-white bg-green-600 hover:bg-green-800 font-mono'>Save</Button></Link>
+                    ) : (
+                        <Link href='/sign-in'><Button className='w-full md:w-auto text-white bg-green-600 hover:bg-green-800 font-mono'>Save</Button></Link>
+                    )
+                }
             </div>
         </nav>
     )
